187546cb5521db33bb417102b2d508c42de9d306
tuowang <2461233967@qq.com>
2026-04-04 13:31:56 +0800

    feat: full sft

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 # pre_train
 CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node=4 trainer/train_pretrain.py --use_wandb \
   --batch_size 130 \
-  --epochs 2 \
+  --epochs 3 \
   --num_hidden_layers 8 \
   --hidden_size 768 \
   --data_path /root/autodl-tmp/pretrain_t2t.jsonl \
   --log_interval 50 \
+  --from_weight pretrain \
   --use_compile 1 && /usr/bin/shutdown  
 # CUDA_VISIBLE_DEVICES=0 python trainer/trainer_pretrain2.py --use_wandb \
 #   --batch_size 16 \
